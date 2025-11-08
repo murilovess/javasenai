dc4d9f079b3693fcd46215a0777f8525973bc048
murilovess <161042765+murilovess@users.noreply.github.com>
2025-11-07 23:56:46 -0300

    Add files via upload

---
+++ UsuarioSpringbootApplication.java
@@ -0,0 +1,11 @@
+package com.example.usuarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsuarioSpringbootApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(UsuarioSpringbootApplication.class, args);
+    }
+}
